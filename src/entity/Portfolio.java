079566611a45721f965496635b1e49140cf93742
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-04 19:50:26 -0400

    implemented Portflio and User

--- src/entity/Portfolio.java
+++ src/entity/Portfolio.java
@@ -1,4 +1,22 @@
 package src.entity;
 
+import java.util.ArrayList;
+
 public class Portfolio {
+    private double value;
+    private double cash;
+    private ArrayList<Holding> holdings;
+
+    public Portfolio (double StartingCash){
+        value = StartingCash;
+        cash = StartingCash;
+    }
+
+    public void setCash(double cash) {
+        this.cash = cash;
+    }
+
+    public double getCash() {
+        return cash;
+    }
 }
--- src/entity/User.java
+++ src/entity/User.java
@@ -1,4 +1,15 @@
 package src.entity;
 
 public class User {
+    private int id;
+    private String username;
+    private String password;
+    private Portfolio portfolio;
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        portfolio = new Portfolio(1000000);
+    }
 }
