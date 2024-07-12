a9885474ee9ae5dcca58d8667b41477627bbbc07
MeerSisodia1707 <meersisodia170703@gmail.com>
2024-07-12 08:54:01 -0400

    Completed API Call.

--- src/data_access/TiingoAPIDataAccessObject.java
+++ src/data_access/TiingoAPIDataAccessObject.java
@@ -3,28 +3,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class TiingoAPIDataAccessObject {
-    private final String API_KEY = "bf408e3ee6e0a153be501f96b468efecc351c902";
-    private String ticker;
+    private final String ticker;
+    private final Double close;
+    private final String date;
+
     public TiingoAPIDataAccessObject(String ticker) {
         this.ticker = ticker;
+        JSONObject jsonObject = this.getAPIData(this.ticker);
+        this.close = this.parseClosePrice(jsonObject);
+        this.date = this.parseDate(jsonObject);
 
     }
 
-    private HashMap<String, Integer> getAPIData(String ticker) {
-        // Create a new HashMap
-        HashMap<String, Integer> map = new HashMap<>();
+    private JSONObject getAPIData(String ticker) {
+        JSONObject jsonObject = null;
         try {
-
-            String urlString = "https://api.tiingo.com/tiingo/daily/" + this.ticker + "/prices";
+            String urlString = "https://api.tiingo.com/tiingo/daily/" + ticker + "/prices";
             URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             // Add request headers
             connection.setRequestProperty("Accept", "application/json");
+            String API_KEY = "bf408e3ee6e0a153be501f96b468efecc351c902";
             connection.setRequestProperty("Authorization", "Token " + API_KEY);
 
             int responseCode = connection.getResponseCode();
@@ -32,27 +38,50 @@ public class TiingoAPIDataAccessObject {
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String inputLine;
                 StringBuilder response = new StringBuilder();
+
                 while ((inputLine = in.readLine()) != null) {
                     response.append(inputLine);
                 }
                 in.close();
+                String rawResponse = response.toString();
+                System.out.println(rawResponse);
+                JSONArray jsonArray = new JSONArray(rawResponse);
+                jsonObject = jsonArray.getJSONObject(0);
 
-                // Print the response
-                System.out.println(response.toString());
             } else {
                 System.out.println("GET request not worked. Response Code: " + responseCode);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return jsonObject;
+    }
 
-        // Add some key-value pairs to the map
-        map.put("Apple", 3);
-        map.put("Banana", 5);
-        map.put("Orange", 2);
+    private Double parseClosePrice(JSONObject jsonObject) {
+        return jsonObject.getDouble("close");
+    }
 
-        // Return the map
-        return map;
+    private String parseDate(JSONObject jsonObject) {
+        return jsonObject.getString("date");
+    }
+
+    public Double getClose() {
+        return close;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public static void main(String[] args) {
+        TiingoAPIDataAccessObject dao = new TiingoAPIDataAccessObject("MSFT");
+        System.out.println(dao.getClose());
+        System.out.println(dao.getDate());
+        System.out.println(dao.getTicker());
     }
 
 
