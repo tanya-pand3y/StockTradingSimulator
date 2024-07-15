79f5c1f04d79cf5135794932795574872c9a60a0
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-14 21:06:49 -0400

    implemented class LoginInputData in use_case

--- src/use_case/login/LoginInputData.java
+++ src/use_case/login/LoginInputData.java
@@ -5,5 +5,12 @@ public class LoginInputData {
     final private String username;
     final private String password;
 
+    public LoginInputData(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
+    String getUsername() {return username;}
+
+    String getPassword() {return password;}
 }
