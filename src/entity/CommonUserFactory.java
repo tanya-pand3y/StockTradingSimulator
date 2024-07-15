150f8e50174791edd7df91889be32dce22bb89ae
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-14 20:11:20 -0400

    fixed a bug in SignupInteractor in use_case

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -5,13 +5,14 @@ import java.time.LocalDateTime;
 public class CommonUserFactory implements UserFactory {
     /**
      * Requires: password is valid.
+     *
      * @param name
      * @param password
      * @return
      */
 
     @Override
-    public User create(String name, String password, LocalDateTime ltd) {
+    public src.entity.User create(String name, String password, LocalDateTime ltd) {
         return new CommonUser(name, password, ltd);
     }
 }
