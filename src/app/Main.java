9906c452a5d220dc99450a6e773270cdebe9c0ef
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-16 19:38:59 -0400

    implementing LoginPresenter

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,7 +2,7 @@ package src.app;
 
 import interface_adapter.login.LoginViewModel;
 import interface_adapter.signup.SignupViewModel;
-import interface_adapter.signup.ViewManagerModel;
+import interface_adapter.ViewManagerModel;
 import src.view.LoginView;
 import src.view.SignupView;
 import src.view.ViewManager;
