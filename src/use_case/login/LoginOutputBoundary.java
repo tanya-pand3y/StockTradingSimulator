f14d3ea85a3377fbb8836098a31ba7fff1180feb
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-16 16:04:22 -0400

    implemented class LoginOutputBoundary in use_case

--- src/use_case/login/LoginOutputBoundary.java
+++ src/use_case/login/LoginOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.login;
 
+import use_case.signup.SignupOutputData;
+
 public interface LoginOutputBoundary {
+    void prepareSuccessView(SignupOutputData user);
+
+    void prepareFailView(String error);
 }
