66bbfcfb7e74b01ca0f5c73d71c9428c755fb569
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-16 16:32:42 -0400

    implemented LoginInteractor i think there is a bug

--- src/use_case/login/LoginInteractor.java
+++ src/use_case/login/LoginInteractor.java
@@ -1,14 +1,36 @@
 package use_case.login;
 
-import java.time.LocalDateTime;
+import src.entity.User;
 
-public class LoginInteractor implements LoginInputBoundary{
+public class LoginInteractor implements LoginInputBoundary {
     final UserLoginDataAccessInterface userDataAccessObject;
-    final LoginOutputBoundary userPresenter;
+    final LoginOutputBoundary loginPresenter;
 
-    public LoginInteractor(LoginUserDataAccessInterface userDataAccessInterface,
+    public LoginInteractor(UserLoginDataAccessInterface userDataAccessInterface,
                            LoginOutputBoundary loginOutputBoundary) {
-        this.userDataAccessObject = userLoginDataAccessInterface;
+        this.userDataAccessObject = userDataAccessInterface;
         this.loginPresenter = loginOutputBoundary;
+    }
+
+    @Override
+    public void execute(LoginInputData loginInputData) {
+
+        String username = loginInputData.getUsername();
+        String password = loginInputData.getPassword();
+
+        if (!userDataAccessObject.existsByName(username)) {
+            loginPresenter.prepareFailView(username + ": Account does not exist.");
+        } else {
+            String pwd = userDataAccessObject.get(username).getPassword();
+            if (!password.equals(pwd)) {
+                loginPresenter.prepareFailView("Incorrect password for " + username + ".");
+            } else {
+
+                User user = userDataAccessObject.get(loginInputData.getUsername());
+
+                LoginOutputData loginOutputData = new LoginOutputData(user.getName(), false);
+                loginPresenter.prepareSuccessView(loginOutputData);
+            }
+        }
     }
 }
