474f0c3ab814219aedabe78b02b11fc72e4854c0
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-16 19:38:52 -0400

    implementing LoginPresenter

--- src/interface_adapter/login/LoginPresenter.java
+++ src/interface_adapter/login/LoginPresenter.java
@@ -1,4 +1,13 @@
 package interface_adapter.login;
 
-public class LoginPresenter {
+import interface_adapter.ViewManagerModel;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LoginPresenter implements LoginOutputBoundary {
 }
