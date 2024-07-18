b1e54dfc036c23edebc7c196df6886857f000534
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-18 12:51:02 -0400

    implemented LoginController in interface_adapters

--- src/interface_adapter/login/LoginController.java
+++ src/interface_adapter/login/LoginController.java
@@ -1,4 +1,18 @@
 package interface_adapter.login;
 
+import use_case.login.LoginInputBoundary;
+import use_case.login.LoginInputData;
+
 public class LoginController {
+
+    private final LoginInputBoundary userLoginUseCaseInteractor;
+
+    public LoginController(LoginInputBoundary userLoginUseCaseInteractor) {
+        this.userLoginUseCaseInteractor = userLoginUseCaseInteractor;
+    }
+
+    public void execute(String username, String password) {
+        LoginInputData loginInputData = new LoginInputData(username, password);
+        userLoginUseCaseInteractor.execute(loginInputData);
+    }
 }
