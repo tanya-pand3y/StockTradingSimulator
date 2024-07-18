2300a47533284f17dd843a574769148ac53108ad
tanya-pand3y <tanyapandey19@gmail.com>
2024-07-18 12:51:16 -0400

    implemented LoginPresenter in interface_adapters

--- src/interface_adapter/login/LoginPresenter.java
+++ src/interface_adapter/login/LoginPresenter.java
@@ -1,14 +1,40 @@
 package interface_adapter.login;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.login.LoginState;
-import interface_adapter.login.LoginViewModel;
 import use_case.login.LoginOutputBoundary;
 import use_case.login.LoginOutputData;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import use_case.signup.SignupOutputData;
 
 public class LoginPresenter implements LoginOutputBoundary {
-    // test
+
+    private final LoginViewModel loginViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel, LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the main view or another appropriate view.
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(response.getUsername());
+        loginState.setLoggedIn(true);
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView("MainView"); // Assuming "MainView" is the view you switch to after login
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData user) {
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setLoginError(error);
+        loginViewModel.firePropertyChanged();
+    }
 }
