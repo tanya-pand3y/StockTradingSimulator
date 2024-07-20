2c7a147f60ba2fd10ac7153bc2973d59ed9f1dd7
ishaaqpanesar <ishaaq.panesar@mail.utoronto.ca>
2024-07-20 18:11:05 -0400

    Test cases

--- src/use_case/stock_history/ViewStockHistoryOutputBoundary.java
+++
@@ -1,9 +0,0 @@
-//package use_case.stock_history;
-//
-//public interface ViewStockHistoryOutputBoundary {
-//    /**
-//     * Presents the result of viewing stock history.
-//     * @param outputData the output data of the stock history view.
-//     */
-//    void present(ViewStockHistoryOutputData outputData);
-//}
